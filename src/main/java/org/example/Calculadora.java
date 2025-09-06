0c01e9a0b3179dde1d00cf1ce95135e44dd062b3
lucasgls <lucasglsilva7@gmail.com>
2025-09-06 11:39:19 -0300

    add: projeto calculadora em java

---
+++ src/main/java/org/example/Calculadora.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Calculadora {
+
+    public static double soma(double numero1, double numero2){
+        return numero1 + numero2;
+    }
+
+    public static double subtrai (double numero1, double numero2){
+        return numero1 - numero2;
+    }
+
+    public static double multiplica (double numero1, double numero2){
+        return numero1 * numero2;
+    }
+
+   public static double divide (double numero1, double numero2){
+        return numero1 / numero2;
+    }
+
+}
